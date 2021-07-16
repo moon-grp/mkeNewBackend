e6725a0d6ead77a828bd4f3df204a72a4c5f623c
emohex <olumidemm@gmail.com>
2021-07-16 17:39:25 +0100

    cors

--- main.py
+++ main.py
@@ -6,9 +6,22 @@ from routes.autos.autosendpoint import auto
 from routes.autos.autosuser import userAuto
 from routes.users.affilendpoint import aff
 from routes.admin.auth import superAdmin
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(frame, prefix="/api/v1/admin")
 app.include_router(userFrame, prefix="/api/v1/users")
 app.include_router(order, prefix="/api/v1/admin")
